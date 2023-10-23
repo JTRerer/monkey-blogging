7d41a8bfae6af88d80184c85d58119761d1c2fb4
JTRerer <trongan147895@gmail.com>
2023-10-23 14:32:12 +0700

    Update 2023/10/23

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,12 +5,16 @@ import styled from "styled-components";
 import Header from "../components/layout/Header";
 import HomeBanner from "../components/module/home/HomeBanner";
 import Layout from "../components/layout/Layout";
+import HomeFeature from "../components/module/home/HomeFeature";
+import HomeNewest from "../components/module/home/HomeNewest";
 const HomePageStyles = styled.div``;
 const HomePage = () => {
   return (
     <HomePageStyles>
       <Layout>
         <HomeBanner></HomeBanner>
+        <HomeFeature></HomeFeature>
+        <HomeNewest></HomeNewest>
       </Layout>
     </HomePageStyles>
   );
