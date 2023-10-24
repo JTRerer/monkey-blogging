d62d9a6c02b5f029384d3d2b5b2021f3bd170637
Toannd832 <toan03182@gmail.com>
2023-10-24 19:12:23 +0700

    first commit

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import axios from 'axios';
 import { getFirestore } from "firebase/firestore";
 import {
   getAuth,
@@ -19,6 +20,7 @@ const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
 getRedirectResult(auth)
   .then((result) => {
     const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -32,21 +34,67 @@ getRedirectResult(auth)
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
+  const baseUrl = 'https://swdprojectapi.azurewebsites.net/api';
 
-export const signInWithGoogle = () => {
-  signInWithRedirect(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user.displayName;
-      const email = result.user.email;
-      const profilePic = result.user.photoURL;
+  async function fetchJwtToken(email) {
+    const url = `${baseUrl}/accounts/login`;
+    const data = JSON.stringify({ email });
+  
+    try {
+      const response = await axios.post(url, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.status === 200) {
+        const { accessToken, id, role } = response.data;
+        // Làm gì đó với accessToken, id và role
+        console.log(accessToken, id, role);
+        return accessToken;
+      } else {
+        throw new Error(`Failed to fetch JWT token ${response.status}`);
+      }
+    } catch (error) {
+      throw new Error(`Failed to fetch JWT token: ${error.message}`);
+    }
+  }
+  
+  // Sử dụng hàm fetchJwtToken
+  const email = 'example@email.com'; // Thay thế bằng email thực tế
+  fetchJwtToken(email)
+    .then(accessToken => {
+      console.log(`JWT Token: ${accessToken}`);
+      // Tiếp tục xử lý sau khi có accessToken
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
+    .catch(error => {
+      console.error(error.message);
     });
-};
+  export const signInWithGoogle = () => {
+    signInWithRedirect(auth, provider)
+      .then((result) => {
+        const email = result.user.email;
+        const displayName = result.user.displayName;
+        const photoURL = result.user.photoURL;
+  
+        // Gọi hàm fetchJwtToken(email) để lấy JWT token từ máy chủ
+        fetchJwtToken(email)
+          .then((jwtToken) => {
+            // Ở đây bạn có thể sử dụng jwtToken theo nhu cầu của mình
+            console.log(jwtToken);
+          })
+          .catch((error) => {
+            // Xử lý lỗi khi lấy JWT token
+            console.error(error);
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const customDataEmail = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // Xử lý lỗi khi đăng nhập
+        console.error(error);
+      });
+  };
+  
